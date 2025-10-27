902a7121933de52b74f1183877438337939e72ac
Manayer <manayerbusiness@gmail.com>
2025-10-27 12:47:42 +0300

    write a models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    role = models.CharField(max_length=50,blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ('planned', 'Planned'),
+        ('active', 'Active'),
+        ('completed','Completed'),
+        ('archived','Archived'),
+    ]
+    
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='planned')
+    owner =models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
