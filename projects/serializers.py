65c782a9214a00411dcd4ae213f319ca507001a6
Manayer <manayerbusiness@gmail.com>
2025-10-29 14:47:28 +0300

    add some code in serializers file for JWT

--- projects/serializers.py
+++ projects/serializers.py
@@ -6,7 +6,26 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+        
+# -----------------------------------------------------------------
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,required=True,min_length=6)
+    
+    class Meta: 
+        model =User
+        fields = ['id','username','email','password']
+        
+    def create(self,validated_data):
+        user= User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email',''),
+            password=validated_data['password']
+        )
+        return user
+        
+        
+# ---------------------------------------------------------------------
 class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
 
@@ -21,12 +40,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         else:
             raise serializers.ValidationError("User must be logged in to create profile.")
         return super().create(validated_data)
+    
+# ----------------------------------------------------------------------------------------------------
         
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)  
     class Meta:
         model = Comment
-        fields = ['task', 'content', 'author']
+        fields = ['id', 'task', 'content', 'author', 'created_at']
 
     def create(self, validated_data):
         user = self.context['request'].user
@@ -36,12 +57,16 @@ class CommentSerializer(serializers.ModelSerializer):
             validated_data['author'] = None  
         return super().create(validated_data)
     
+# ---------------------------------------------------------------------------------------------------------
+    
 class TaskSerializer(serializers.ModelSerializer):
     assigned_to = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = ['id', 'title', 'due_date', 'status', 'project', 'assigned_to', 'created_at', 'updated_at', 'comments']
+        
+# -------------------------------------------------------------------------------------------------------------------------
 
 class ProjectSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
