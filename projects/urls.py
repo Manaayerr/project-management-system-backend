cf09e16fd0688e0625b711feb30cc4a1c954f473
Manayer <manayerbusiness@gmail.com>
2025-10-30 10:29:21 +0300

    solve a urls for token in postman

--- projects/urls.py
+++ projects/urls.py
@@ -1,18 +1,10 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
 from rest_framework import routers
 from .views import ProjectViewSet, TaskViewSet, CommentViewSet, UserProfileViewSet
 
 router = routers.DefaultRouter()
-router.register(r'projects', ProjectViewSet)
-router.register(r'tasks', TaskViewSet)
-router.register(r'comments', CommentViewSet)
-router.register(r'profiles', UserProfileViewSet)
+router.register('projects', ProjectViewSet)
+router.register('tasks', TaskViewSet)
+router.register('comments', CommentViewSet)
+router.register('profiles', UserProfileViewSet)
 
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(router.urls)),  
-]
+urlpatterns = router.urls
