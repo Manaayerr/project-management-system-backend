93c917b8c01cbeeeb4e41f3f019ca0f0d7c86d74
Manayer <manayerbusiness@gmail.com>
2025-11-02 14:19:13 +0300

    add nested task routes under projects for better REST structure

--- projects/urls.py
+++ projects/urls.py
@@ -15,7 +15,13 @@ router.register('comments', CommentViewSet)
 router.register('profiles', UserProfileViewSet)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'), 
+    path('register/', RegisterView.as_view(), name='register'),
+    path('projects/<int:project_pk>/tasks',TaskViewSet.as_view({'get':'list','post': 'create'}),
+        name='project-tasks-list'),
+    
+    path('projects/<int:project_pk>/tasks/<int:pk>/',TaskViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), 
+        name='project-task-detail'
+    ),
 ]
 
 urlpatterns += router.urls
